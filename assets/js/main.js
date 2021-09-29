d2bdc93eab3313e9aef941e8f349e9a506012e13
Abhinavpanwarrajput <76612120+Abhinavpanwarrajput@users.noreply.github.com>
2021-09-29 21:16:59 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,7 @@
-/*
-	Hyperspace by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
-
+let nam = prompt("Enter Your Name");
+if(nam == null){
+	topName.innerHTML = ("You");	
+}else{topName.innerHTML = nam;}
 (function($) {
 
 	var	$window = $(window),
